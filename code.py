0786a8c3d7ece804d55db6d55b8078929ac6a1c9
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-11 19:17:13 +0530

    Hough Circles Transform

--- code.py
+++ code.py
@@ -1,35 +1,19 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('sudoku.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img = cv2.imread('opencvLogo.png', 0)
+img = cv2.medianBlur(img, 5)
+cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
+circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20)
 
-'''
-lines = cv2.HoughLines(edges,1,np.pi/180,140)
-for line in lines:
-	for rho,theta in line:
-		a = np.cos(theta)
-		b = np.sin(theta)
-		x0 = a*rho
-		y0 = b*rho
-		x1 = int(x0 + 1000*(-b))
-		y1 = int(y0 + 1000*(a))
-		x2 = int(x0 - 1000*(-b))
-		y2 = int(y0 - 1000*(a))
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-		cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-'''
-
-minLineLength = 150
-maxLineGap = 10
-
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-for line in lines:
-	for x1,y1,x2,y2 in line:
-		cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-
-cv2.imshow('window', img)
+cv2.imshow('detected circles',cimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
