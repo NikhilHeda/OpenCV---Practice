372cddd4f4bb24213cbe735808d24f17a8b2e82e
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-11 14:40:21 +0530

    Smoothing Images

--- code.py
+++ code.py
@@ -2,71 +2,42 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Simple Thresholding
-# cv2.threshold()
+img = cv2.imread('opencvLogo.png')
+
+# 2D Convolution (Image Filtering)
 '''
-img = cv2.imread('image.png', 0)
-ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
-ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
-ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
-ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
-
-titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
-
-for i in range(6):
-	plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
-	plt.title(titles[i])
-	plt.xticks([]), plt.yticks([])
-
-plt.show()
+kernel = np.ones((5,5),np.float32)/25
+dst = cv2.filter2D(img,-1,kernel)
 '''
 
-# Adaptive Thresholding
-# cv2.adaptiveThreshold()
+# Image Blurring
+# 1. Averaging
+# cv2.blur()
 '''
-img = cv2.imread('image.png', 0)
-ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-thresh2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-thresh3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-titles = ['Original Image', 'Global Thresholding (v = 127)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-images = [img, thresh1, thresh2, thresh3]
-
-for i in range(4):
-    plt.subplot(2, 2, i+1), plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
-    plt.xticks([]), plt.yticks([])
-plt.show()
+dst = cv2.blur(img, (9, 9))
 '''
 
-# Otsu's Binarization
-img = cv2.imread('image.png', 0)
+# Gaussian Blurring
+# cv2.GaussianBlur()
+# cv2.getGaussianKernel()
+'''
+dst = cv2.GaussianBlur(img, (5, 5), 0)
+'''
 
-# global thresholding
-ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# Median Blurring
+# cv2.medianBlur()
+'''
+dst = cv2.medianBlur(img, 5)
+'''
 
-# Otsu's thresholding
-ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# Bilateral Filtering
+# cv2.bilateralFilter()
 
-# Otsu's thresholding after Gaussian filtering
-blur = cv2.GaussianBlur(img,(5,5),0)
-ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+dst = cv2.bilateralFilter(img, 9, 75, 75)
 
-# plot all the images and their histograms
-images = [img, 0, th1,
-          img, 0, th2,
-          blur, 0, th3]
-titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
-          'Original Noisy Image','Histogram',"Otsu's Thresholding",
-          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
 
-for i in range(3):
-    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
-    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
-    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
-    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
-    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
-    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
+plt.xticks([]), plt.yticks([])
 plt.show()
