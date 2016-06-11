f34f8daf02e1b7fc9b7c7d390289873c4e260020
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-11 15:15:38 +0530

    Image Pyramids

--- code.py
+++ code.py
@@ -2,12 +2,59 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('opencvLogo.png',0)
-edges = cv2.Canny(img,100,200)
+# Image Pyramids
+# cv2.pyrUp()
+# cv2.pyrDown()
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+A = cv2.imread('apple.jpg')
+B = cv2.imread('orange.jpg')
 
-plt.show()
+A = cv2.resize(A, (200, 200), interpolation = cv2.INTER_CUBIC)
+B = cv2.resize(B, (200, 200), interpolation = cv2.INTER_CUBIC)
+
+# generate Gaussian pyramid for A
+G = A.copy()
+gpA = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpA.append(G)
+
+# generate Gaussian pyramid for B
+G = B.copy()
+gpB = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpB.append(G)
+
+# generate Laplacian Pyramid for A
+lpA = [gpA[5]]
+for i in range(5, 0, -1):
+    GE = cv2.pyrUp(gpA[i])
+    L = cv2.subtract(gpA[i - 1],GE)
+    lpA.append(L)
+
+# generate Laplacian Pyramid for B
+lpB = [gpB[5]]
+for i in range(5,0,-1):
+    GE = cv2.pyrUp(gpB[i])
+    L = cv2.subtract(gpB[i-1],GE)
+    lpB.append(L)
+
+# Now add left and right halves of images in each level
+LS = []
+for la,lb in zip(lpA,lpB):
+    rows,cols,dpt = la.shape
+    ls = np.hstack((la[:,0:cols/2], lb[:,cols/2:]))
+    LS.append(ls)
+
+# now reconstruct
+ls_ = LS[0]
+for i in range(1,6):
+    ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.add(ls_, LS[i])
+
+# image with direct connecting each half
+real = np.hstack((A[:,:cols/2],B[:,cols/2:]))
+
+cv2.imwrite('Pyramid_blending2.jpg',ls_)
+cv2.imwrite('Direct_blending.jpg',real)
