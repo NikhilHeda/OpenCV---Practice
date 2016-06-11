1b2776cbd719e19d6ec4c6b76ad909a65cd36118
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-11 13:11:01 +0530

    Drawing functions in OpenCV

--- code.py
+++ code.py
@@ -1,108 +1,65 @@
 import numpy as np
 import cv2
 
-# Playing Video from Camera
+img = np.zeros((512, 512, 3), np.uint8)
 
-# cv2.VideoCapture()
-# cv2.cvtColor()
+# colors
+blue = (255, 0, 0)
+green = (0, 255, 0)
+red = (0, 0, 255)
+yellow = (0, 255, 255)
+unknown = (255, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = 10
 
-# VideoCapture Object methods : 
-# isOpened()
-# open()
-# read()
-# release()
 '''
-cap = cv2.VideoCapture(0) # 0, -1 : for one camera
+# line
+cv2.line(img, (0, 0), (511, 511), blue, 5)
+cv2.line(img, (511, 0), (0, 511), blue, 5)
 
-while True:
-	if not cap.isOpened():
-		cap.open()
-	
-	ret, frame = cap.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
-	cv2.imshow('frame', gray)
-	
-	# if cv2.waitKey(1) & 0xFF == ord('q'):
-	#	break
-	# or
-	
-	k = cv2.waitKey(1) # 1 millisecod wait to display image, not 0 because 0 is indefinite wait.
-	if  k == ord('q'):
-		break
+# rectangle
+cv2.rectangle(img, (127, 127), (383, 383), green, 3)
 
-cap.release()
-cv2.destroyAllWindows()
-'''
+# circle
+cv2.circle(img, (254, 254), 128, red, 3, cv2.LINE_AA)
 
-# VideoCapture Object methods : 
-# get()
-# set()
-'''
-cap = cv2.VideoCapture(0)
+# ellipse
+cv2.ellipse(img, (254, 304), (256, 128), 0, 0, 180, yellow, 3, cv2.LINE_AA)
+cv2.ellipse(img, (254, 204), (256, 128), 180, 0, 180, yellow, 3, cv2.LINE_AA)
 
-while cap.isOpened:
-	
-	ret, frame = cap.read()
-	
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
-	
-	ret = cap.set(3, 320)	# frame width
-	ret = cap.set(4, 240) # frame height
-	
-	cv2.imshow('frame', gray)
-	
-	k = cv2.waitKey(1)
-	if  k == ord('q'):
-		break
+# polygon
+pts = np.array([ [254, 0], [447, 254], [254, 511], [63, 254]], np.int32)
+pts = np.reshape(pts, (-1, 1, 2))
+cv2.polylines(img, [pts], True, unknown)
 
-cap.release()
-cv2.destroyAllWindows()
+# text
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'Open CV', (50, 500), font, 3, white, 2, cv2.LINE_AA)
 '''
 
-# Playing Video from file
-'''
-cap = cv2.VideoCapture('video.mp4')
+# Open CV logo
+img = np.zeros((612, 512, 3), np.uint8) 
+img[:] = white	# setting each pixel to white
 
-while cap.isOpened:
-	
-	ret, frame = cap.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
-	cv2.imshow('frame', gray)
-	
-	k = cv2.waitKey(25)
-	if  k == ord('q'):
-		break
+circle_radius = 118
 
-cap.release()
-cv2.destroyAllWindows()
-'''
+start_angle = 180
+cv2.ellipse(img, (255, 127), (circle_radius, circle_radius), start_angle, -60, 240, red, -1, cv2.LINE_AA)
+cv2.ellipse(img, (255, 127), (circle_radius - 70, circle_radius - 70), start_angle, -62, 242, white, -1, cv2.LINE_AA)
 
-# Saving a Video
+start_angle = 60
+cv2.ellipse(img, (127, 348), (circle_radius, circle_radius), start_angle, -60, 240, green, -1, cv2.LINE_AA)
+cv2.ellipse(img, (127, 348), (circle_radius - 70, circle_radius - 70), start_angle, -62, 242, white, -1, cv2.LINE_AA)
 
-# cv2.VideoWriter()
-# cv2.VideoWriter_fourcc()
-# cv2.flip()
+start_angle = 0
+cv2.ellipse(img, (383, 348), (circle_radius, circle_radius), start_angle, -60, 240, blue, -1, cv2.LINE_AA)
+cv2.ellipse(img, (383, 348), (circle_radius - 70, circle_radius - 70), start_angle, -62, 242, white, -1, cv2.LINE_AA)
 
-# VideoWriter Object methods : 
-# write()
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'OpenCV', (75, 550), font, 3, black, 4, cv2.LINE_AA)
 
-cap = cv2.VideoCapture(0)
-
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-out = cv2.VideoWriter('saved_video.mp4', fourcc, 20.0, (640, 480) )
-
-while cap.isOpened():
-	ret, frame = cap.read();
-	if ret:
-		frame = cv2.flip(frame, 1) # 1 : flip horizontally; 0 : flip vertically
-		cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		cv2.imshow('frame',frame)
-		out.write(frame)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-	else:
-		break
-
-cap.release()
-out.release()
+cv2.imshow('My Drawing', img)
+cv2.imwrite('opencvLogo.png', img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
