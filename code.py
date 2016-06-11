de256bea04bf6010f0a60eb2f07bc8a3e410af6e
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-11 19:40:13 +0530

    Image Segmentation with Watershed Algorithm

--- code.py
+++ code.py
@@ -1,19 +1,37 @@
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-img = cv2.imread('opencvLogo.png', 0)
-img = cv2.medianBlur(img, 5)
-cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+img = cv2.imread('water_coins.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20)
+ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)# noise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+# sure background area
+sure_bg = cv2.dilate(opening,kernel,iterations=3)
 
-cv2.imshow('detected circles',cimg)
+# Finding sure foreground area
+dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg,sure_fg)
+
+# Marker labelling
+ret, markers = cv2.connectedComponents(sure_fg)
+
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+
+markers = cv2.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+
+cv2.imshow('window', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
