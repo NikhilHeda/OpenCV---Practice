30fa163f791fa33e8e3554b401b011e4004da821
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-11 13:24:15 +0530

    Performance Measurement and Improvement Techniques

--- code.py
+++ code.py
@@ -1,53 +1,32 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
-# Image Addition
-'''
-x = np.uint8([250])
-y = np.uint8([10])
-print(cv2.add(x, y))		# 250 + 10 = 260 => 255
-print(x + y)				# 250 + 10 = 260 % 256 => 4
-'''
+# cv2.getTickCount()
+# cv2.getTickFrequency()
 
-# Image Blending
-'''
 img1 = cv2.imread('image.png')
-img2 = cv2.imread('opencvLogo.png')
-img1 = cv2.resize(img1, (500, 500), interpolation = cv2.INTER_CUBIC)
-img2 = cv2.resize(img2, (500, 500), interpolation = cv2.INTER_CUBIC)
-dst = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
-cv2.imshow('window', dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+e1 = cv2.getTickCount()
+for i in range(5, 49, 2):
+	img1 = cv2.medianBlur(img1, i)
+e2 = cv2.getTickCount()
+print( (e2 - e1) / cv2.getTickFrequency() )
+
+# IPython
 '''
+# check if optimization is enabled
+In [5]: cv2.useOptimized()
+Out[5]: True
 
-# Bitwise Operators
-img1 = cv2.imread('image.png')
-img2 = cv2.imread('opencvLogo.png')
-img2 = cv2.resize(img2, (100, 100), interpolation = cv2.INTER_CUBIC)
-rows, cols = img2.shape[:2]
-roi = img1[0:rows, 0:cols]
+In [6]: %timeit res = cv2.medianBlur(img,49)
+10 loops, best of 3: 34.9 ms per loop
 
-imgray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(imgray, 155, 255, cv2.THRESH_BINARY_INV)
-ret, mask_inv = cv2.threshold(imgray, 155, 255, cv2.THRESH_BINARY)
+# Disable it
+In [7]: cv2.setUseOptimized(False)
 
-img1_bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
-img2_fg = cv2.bitwise_and(img2, img2, mask = mask)
+In [8]: cv2.useOptimized()
+Out[8]: False
 
-dst = cv2.add(img1_bg,img2_fg)
-img1[0:rows, 0:cols ] = dst
+In [9]: %timeit res = cv2.medianBlur(img,49)
+10 loops, best of 3: 64.1 ms per loop
 '''
-cv2.imshow('window', dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
-plt.subplot(231), plt.imshow(mask), plt.xticks([]), plt.yticks([])
-plt.subplot(232), plt.imshow(mask_inv), plt.xticks([]), plt.yticks([])
-plt.subplot(233), plt.imshow(img1_bg), plt.xticks([]), plt.yticks([])
-plt.subplot(234), plt.imshow(img2_fg), plt.xticks([]), plt.yticks([])
-plt.subplot(235), plt.imshow(dst), plt.xticks([]), plt.yticks([])
-plt.subplot(236), plt.imshow(img1), plt.xticks([]), plt.yticks([])
-
-plt.show()
