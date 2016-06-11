0dfa94dda3abdd3a33856f35fe393a5d66a1fa1d
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-11 17:10:48 +0530

    Template Matching

--- code.py
+++ code.py
@@ -3,114 +3,36 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('dog.jpg',0)
+img2 = img.copy()
+template = cv2.imread('dog_template.jpg',0)
+w, h = template.shape[::-1]
 
-# Fourier Transform in Numpy
-'''
-# Magnitude Spectrum
-# np.fft.fft2()
-# np.fft.fftshift()
+# All the 6 methods for comparison in a list
+methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
 
-f = np.fft.fft2(img)
-fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20*np.log(np.abs(fshift))
+for meth in methods:
+	img = img2.copy()
+	method = eval(meth)
 
-plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-plt.show()
+	# Apply template Matching
+	res = cv2.matchTemplate(img,template,method)
+	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-# Inverse Shift and Inverse FFT
-# np.fft.ifft2()
-# np.fft.ifftshift()
+	# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+	if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+		top_left = min_loc
+	else:
+		top_left = max_loc
+	
+	bottom_right = (top_left[0] + w, top_left[1] + h)
 
-rows, cols = img.shape
-crow,ccol = rows/2 , cols/2
-fshift[crow-30:crow+30, ccol-30:ccol+30] = 0
-f_ishift = np.fft.ifftshift(fshift)
-img_back = np.fft.ifft2(f_ishift)
-img_back = np.abs(img_back)
+	cv2.rectangle(img,top_left, bottom_right, 255, 2)
 
-plt.subplot(131),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
-plt.title('Image after High Pass Filtering'), plt.xticks([]), plt.yticks([])
-plt.subplot(133),plt.imshow(img_back)
-plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+	plt.subplot(121),plt.imshow(res,cmap = 'gray')
+	plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+	plt.subplot(122),plt.imshow(img,cmap = 'gray')
+	plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+	plt.suptitle(meth)
 
-plt.show()
-'''
-
-# Fourier Transform in OpenCV
-# Magnitude Spectrum
-# cv2.dft()
-'''
-dft = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
-dft_shift = np.fft.fftshift(dft)
-
-magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:,:,0], dft_shift[:,:,1]))
-
-plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-plt.show()
-
-rows, cols = img.shape
-crow,ccol = rows/2 , cols/2
-
-# create a mask first, center square is 1, remaining all zeros
-mask = np.zeros((rows,cols,2),np.uint8)
-mask[crow-30:crow+30, ccol-30:ccol+30] = 1
-
-# apply mask and inverse DFT
-fshift = dft_shift*mask
-f_ishift = np.fft.ifftshift(fshift)
-img_back = cv2.idft(f_ishift)
-img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])
-
-plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
-plt.title('Output Spectrum'), plt.xticks([]), plt.yticks([])
-plt.show()
-'''
-
-# Why Laplacian is a high pass filter
-# simple averaging filter without scaling parameter
-mean_filter = np.ones((3,3))
-
-# creating a guassian filter
-x = cv2.getGaussianKernel(5,10)
-gaussian = x*x.T
-
-# different edge detecting filters
-# scharr in x-direction
-scharr = np.array([[-3, 0, 3],
-                   [-10,0,10],
-                   [-3, 0, 3]])
-# sobel in x direction
-sobel_x= np.array([[-1, 0, 1],
-                   [-2, 0, 2],
-                   [-1, 0, 1]])
-# sobel in y direction
-sobel_y= np.array([[-1,-2,-1],
-                   [0, 0, 0],
-                   [1, 2, 1]])
-# laplacian
-laplacian=np.array([[0, 1, 0],
-                    [1,-4, 1],
-                    [0, 1, 0]])
-
-filters = [mean_filter, gaussian, laplacian, sobel_x, sobel_y, scharr]
-filter_name = ['mean_filter', 'gaussian','laplacian', 'sobel_x', \
-                'sobel_y', 'scharr_x']
-fft_filters = [np.fft.fft2(x) for x in filters]
-fft_shift = [np.fft.fftshift(y) for y in fft_filters]
-mag_spectrum = [np.log(np.abs(z)+1) for z in fft_shift]
-
-for i in range(6):
-    plt.subplot(2,3,i+1),plt.imshow(mag_spectrum[i],cmap = 'gray')
-    plt.title(filter_name[i]), plt.xticks([]), plt.yticks([])
-
-plt.show()
+	plt.show()
